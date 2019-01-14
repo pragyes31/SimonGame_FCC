bf53955e917dddd314382ae1994f44a338832d0b
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-01-14 11:20:08 +0530

    modifying generateColor sequence function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import "./styles.css";
 
 function createSimonGame() {
-  const simonGameApp = {};
+  const colorBtns = Array.from(document.querySelectorAll(".color"));
+  const testBtn = document.querySelector(".testing");
+  const red = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
+  );
+  const blue = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"
+  );
+  const yellow = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"
+  );
+  const green = new Audio(
+    "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+  );
+
+  const simonsMove = [];
+  let sequenceCount = 1;
+  const simonGameApp = {
+    generateColorSequence: () => {
+      let pickRandomColor = Math.floor(Math.random() * colorBtns.length);
+      let currentColor = simonsMove.push(colorBtns[pickRandomColor].innerHTML);
+      sequenceCount++;
+      currentColor.play();
+      console.log(simonsMove);
+    }
+  };
+  testBtn.addEventListener("click", simonGameApp.generateColorSequence);
   return simonGameApp;
 }
 
