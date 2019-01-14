43489e7db6afe0bf61113373260c588ee2f321a9
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-01-14 19:12:34 +0530

    added checkUsersMove function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 function createSimonGame() {
   const colorBtns = Array.from(document.querySelectorAll(".color"));
+  const playBtn = document.querySelector(".play");
   const testBtn = document.querySelector(".testing");
   const red = new Audio(
     "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
@@ -16,18 +17,33 @@ function createSimonGame() {
     "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
   );
 
-  const simonsMove = [];
+  let botPattern = [];
+  let userPattern = [];
   let sequenceCount = 1;
   const simonGameApp = {
     generateColorSequence: () => {
       let pickRandomColor = Math.floor(Math.random() * colorBtns.length);
-      let currentColor = simonsMove.push(colorBtns[pickRandomColor].innerHTML);
-      sequenceCount++;
-      currentColor.play();
-      console.log(simonsMove);
+      let currentColor = botPattern.push(colorBtns[pickRandomColor].innerHTML);
+
+      // currentColor.play();
+      console.log(botPattern);
+    },
+    checkUsersMove: e => {
+      userPattern.push(e.target.innerHTML);
+      let botPatternToTest = botPattern.slice(0, userPattern.length + 1);
+      for (let i = 0; i < userPattern.length; i++) {
+        if (botPatternToTest !== userPattern) {
+          //alert('wrong pattern bruv!! Let me restart the game for you!!!')
+        } else {
+          //do something else
+        }
+      }
     }
   };
-  testBtn.addEventListener("click", simonGameApp.generateColorSequence);
+  playBtn.addEventListener("click", simonGameApp.generateColorSequence);
+  colorBtns.forEach(e =>
+    e.addEventListener("click", simonGameApp.checkUsersMove)
+  );
   return simonGameApp;
 }
 
